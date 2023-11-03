7bf64edfbf7ffb96a847c70ab516306be98e4ec9
yassinehaddoudi <yassine.haddoudi.it@gmail.com>
2023-11-03 09:21:16 +0100

    rename and modify minimal native only neovim config

--- native-init.lua
+++ native-init.lua
@@ -22,38 +22,6 @@ vim.cmd[[let &path = getcwd() . '/**']]
 vim.cmd[[set grepprg=rg\ --vimgrep]]
 vim.cmd[[set grepformat=%f:%l:%c:%m]]
 
--- Mini Plugins: Fuzzy Finder, Autocompletion, Autopairs
--- https://github.com/echasnovski/mini.nvim
-local path = vim.fn.stdpath('data')..'/site/pack/mini.nvim/start/mini.nvim'
-if vim.fn.empty(vim.fn.glob(path)) > 0 then
-	vim.fn.system({'git', 'clone', 'https://github.com/echasnovski/mini.nvim', path})
-end
-
-require('mini.base16').setup({ 
-	palette = {
-		base00= "#282c34",
-		base01= "#353b45",
-		base02= "#3e4451",
-		base03= "#545862",
-		base04= "#565c64",
-		base05= "#abb2bf",
-		base06= "#b6bdca",
-		base07= "#c8ccd4",
-		base08= "#e06c75",
-		base09= "#d19a66",
-		base0A= "#e5c07b",
-		base0B= "#98c379",
-		base0C= "#56b6c2",
-		base0D= "#61afef",
-		base0E= "#c678dd",
-		base0F= "#be5046",
-    },
-})
-require('mini.pick').setup({})
-require('mini.completion').setup({})
-require('mini.pairs').setup({})
-require('mini.comment').setup({})
-
 -- Treesitter: Highlight & Folding
 -- https://github.com/nvim-treesitter/nvim-treesitter
 vim.wo.foldtext = "v:lua.vim.treesitter.foldtext()"
