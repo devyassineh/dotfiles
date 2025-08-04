4cf410642f65207762b8a11cddc8707a36bf273f
yh <98450495+devyassineh@users.noreply.github.com>
2025-08-04 22:40:43 +0200

    Delete Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-Function lint {
-	python -m pylint
-}
-Set-Alias -Name g  -Value git
-Set-Alias -Name pylint  -Value lint
-oh-my-posh init pwsh --config 'C:\Users\Yassine\AppData\Local\Programs\oh-my-posh\themes\avit.omp.json' | Invoke-Expression 
-#Set-PSReadlineOption -EditMode vi
-
